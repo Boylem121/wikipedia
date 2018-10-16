dde5d6ff8d8a3e7a052db78517d42a8831453a78
Michael Boyle <boylem121@gmail.com>
2018-10-16 11:08:47 -0500

    Add comments for clarity

--- scripts.js
+++ scripts.js
@@ -4,20 +4,20 @@
    $('#submitButton').click(function() {
 
      var searchWord = $('#searchBar').val();
-     var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchWord + "&limit=50&namespace=0&format=json";
+     var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchWord + "&limit=50&namespace=0&format=json"; //set up the url to pass in seasrch word
      console.log(url);
 
-     $.ajax({
+     $.ajax({//gets the information from the api
        type: "GET",
        dataType: "jsonp",
        url: url,
        success: function(data) {
          $( ".resault" ).remove();
-         for(var i=0;i<data[1].length;i++){
+         for(var i=0;i<data[1].length;i++){ //displays each resault in its own sepreat div in the body
            $("body").append("<div class='resault col-xs-offset-1 col-xs-10'><a class='resault-title' href='"+data[3][i]+"' target='_blank'>"+data[1][i]+"</a><p class='resault-description'>"+data[2][i]+"</p>");
          }
        },
-       error: function(errorMesage) {
+       error: function(errorMesage) { //if it cant reach the api
          console.log("error");
        }
      });
